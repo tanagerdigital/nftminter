f9cd7e439ebcb735f1c340b01bf6b27dd0ba20b5
liukun <liukundavid@gmail.com>
2022-12-25 21:58:55 +0800

    fix: update SN meta data

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,33 +10,51 @@ const Home: NextPage = () => {
     <div className="min-h-screen">
       <Head>
         <title>Tanager Minter</title>
-        <meta
-          name="twitter:title"
-          property="og:title"
-          content="Tanager Mint Page"
-        />
-        <meta name="twitter:site" content="@tanager2022" />
+        {/* HTML Meta Tags */}
+        <title>Tanager Mint Page</title>
         <meta
           name="description"
           content="2022 年开始，很多传统大品牌与名人都开始发行自己的 NFT ，用来与他们的用户与粉丝互动，NFT 真的能解决他们这几年来停滞不前的营销难题吗？阅读我们的文章告诉你答案。"
         />
-        <meta name="viewport" content="initial-scale=1" />
+
+        {/* Google / Search Engine Tags */}
+        <meta itemprop="name" content="Tanager Mint Page" />
         <meta
-          name="twitter:description"
+          itemprop="description"
+          content="2022 年开始，很多传统大品牌与名人都开始发行自己的 NFT ，用来与他们的用户与粉丝互动，NFT 真的能解决他们这几年来停滞不前的营销难题吗？阅读我们的文章告诉你答案。"
+        />
+        <meta
+          itemprop="image"
+          content="https://tanagerdigital.github.io/nftminter/images/logo.png"
+        />
+
+        {/* Facebook Meta Tags */}
+        <meta
+          property="og:url"
+          content="https://tanagerdigital.github.io/nftminter"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Tanager Mint Page" />
+        <meta
           property="og:description"
           content="2022 年开始，很多传统大品牌与名人都开始发行自己的 NFT ，用来与他们的用户与粉丝互动，NFT 真的能解决他们这几年来停滞不前的营销难题吗？阅读我们的文章告诉你答案。"
         />
         <meta
-          name="twitter:image:src"
           property="og:image"
-          content="/nftminter/images/logo.png"
+          content="https://tanagerdigital.github.io/nftminter/images/logo.png"
         />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content="https://tanagerdigital.github.io/nftminter/"
-        />
+
+        {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Tanager Mint Page" />
+        <meta
+          name="twitter:description"
+          content="2022 年开始，很多传统大品牌与名人都开始发行自己的 NFT ，用来与他们的用户与粉丝互动，NFT 真的能解决他们这几年来停滞不前的营销难题吗？阅读我们的文章告诉你答案。"
+        />
+        <meta
+          name="twitter:image"
+          content="https://tanagerdigital.github.io/nftminter/images/logo.png"
+        />
       </Head>
       <div className={style.header}>
         <Header />

2b87b8a74c60f5da0ceabf1af7a3f673d69dc4d6
liukun <liukundavid@gmail.com>
2022-12-25 21:35:05 +0800

    feat: update header meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import GuidePage from '../components/guide'
 import Header from '../components/header'
 import MintPage from '../components/mint'
@@ -7,6 +8,10 @@ import style from './index.module.css'
 const Home: NextPage = () => {
   return (
     <div className="min-h-screen">
+      <Head>
+        <title>Tanager Minter</title>
+        <meta property="og:title" content="Tanager Mint Page" key="title" />
+      </Head>
       <div className={style.header}>
         <Header />
         <main className="main">

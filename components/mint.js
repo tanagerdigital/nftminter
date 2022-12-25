e9449bc7d7b6ca4c05de19b7bccc136113c89f88
liukun <liukundavid@gmail.com>
2022-12-25 17:28:22 +0800

    feat: 更新MintPage布局

--- components/mint.js
+++ components/mint.js
@@ -2,13 +2,20 @@ import style from './mint.module.css'
 
 const MintPage = () => {
   return (
-    <div className={style.content}>
-      <div className={style.title}>Tanager</div>
-      <div className={style.button_wrapper}>
-        <div className={`${style.title} ${style.button}`}>
-          Mint your 1st NFT
+    <div className="corners">
+      <div className={style.content}>
+        <div className={style.title}>Tanager</div>
+        <div className={style.button_wrapper}>
+          <div className={`${style.title} ${style.button}`}>
+            Mint your 1st NFT
+          </div>
         </div>
       </div>
+      <div className={`${style.topLeft} ${style.corner_button}`} />
+      <div className={`${style.topRight} ${style.corner_button}`} />
+      <div className={`${style.bottomLeft} ${style.corner_button}`} />
+      <div className={`${style.bottomCenter} ${style.arrow_button}`} />
+      <div className={`${style.bottomRight} ${style.corner_button}`} />
     </div>
   )
 }

a2b48eaac1ba0a93200f9c150267563f25865284
liukun <liukundavid@gmail.com>
2022-12-27 10:19:07 +0800

    Deploy gh-pages

---
+++ _next/static/NqglwV76EzU1KIJrm-9Zu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/38-0c828c28957bda7e.js","static/css/a086575322bfc01b.css","static/chunks/pages/index-d09ea46e835c5cd8.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

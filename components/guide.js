e671980e07590e2998ee54454c99f0a7738c1d8c
liukun <liukundavid@gmail.com>
2022-12-27 16:44:12 +0800

    fix: update eng content

--- components/guide.js
+++ components/guide.js
@@ -4,15 +4,15 @@ import Guideline from './guideline'
 const steps = [
   {
     title: '1.Where 2 find my NFT',
-    desc: '当 NFT 显示 mint 成功后，你不会在这个页面看到它。 正确的方式是，访问 OpenSea，在个人资料 - 我的搜集处找到它。',
+    desc: 'You will not see it when you successfully minted your NFT on this page. The correct way is to visit opensea（ NFT exchange）and find it under “Your Profile - My Collection.”',
   },
   {
     title: '2. Visit our Github',
-    desc: '访问 Tanager 的 Github ，你可以参考我们页面的代码，自行开发一套 Mint NFT 网页流程。',
+    desc: "Visit Tanager's Github, where you can refer to our page's code to develop your Mint NFT web flows",
   },
   {
     title: '3. Read our articles about NFT and marketing',
-    desc: '2022 年开始，很多传统大品牌与名人都开始发行自己的 NFT ，用来与他们的用户与粉丝互动，NFT 真的能解决他们这几年来停滞不前的营销难题吗？阅读我们的文章告诉你答案。',
+    desc: 'Since 2022, many traditional big brands and celebrities have issued their own NFTs to engage with their subscribers and fans. However, can NFTs solve their marketing challenges that have been stagnant for years? Could you read our article <xxx > to find out? ',
   },
   {
     title: '4. Get to know Tanager and our Core Developers',

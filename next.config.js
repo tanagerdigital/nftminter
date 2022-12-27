933c23799e719ba5751f0e0509f1dd4d9142a5e4
liukun <liukundavid@gmail.com>
2022-12-27 11:11:17 +0800

    fix: add dotenv; add basePath

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config()
+
+const isProd = process.env.NODE_ENV === 'live'
 module.exports = {
   reactStrictMode: true,
+  basePath: '/nftminter',
+  assetPrefix: isProd
+    ? 'https://tanagerdigital.github.io/nftminter/'
+    : undefined,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 }
